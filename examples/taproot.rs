f3eb50fa482fb28b6313638d380f9d2155049f31
rustaceanrob <rob.netzke@gmail.com>
2026-01-21 21:29:19 +0000

    Add `taproot` example
    Sync from the taproot activation height up to the current chain tip
    according to the server status.

---
+++ examples/taproot.rs
@@ -0,0 +1,32 @@
+use core::time::Duration;
+use std::time::Instant;
+
+const ENDPOINT: block_dn_client::Endpoint<'static> = block_dn_client::Endpoint::BLOCKDNORG;
+const TIMEOUT: Duration = Duration::from_secs(5);
+const TAPROOT_ACTIVATION_HEIGHT: u32 = 700_000;
+
+fn main() {
+    let mut client_builder = block_dn_client::Builder::new();
+    client_builder = client_builder.timeout(TIMEOUT);
+    client_builder = client_builder.endpoint(ENDPOINT);
+    let client = client_builder.build();
+    let status = client.status().unwrap();
+    let now = Instant::now();
+    let mut start_height = TAPROOT_ACTIVATION_HEIGHT;
+    let stop_height = status.best_filter_height;
+    let mut total_bytes = 0;
+    println!("Syncing to height {stop_height}");
+    while start_height < stop_height {
+        let filters = client.filters(start_height).unwrap();
+        start_height += filters.len() as u32;
+        let bytes = filters
+            .into_iter()
+            .map(|filter| filter.content.len())
+            .sum::<usize>();
+        total_bytes += bytes;
+        println!("{start_height}/{stop_height}");
+    }
+    println!("Total bytes downloaded: {total_bytes}");
+    println!("Time elapsed: {} seconds", now.elapsed().as_secs());
+    println!("Done");
+}
